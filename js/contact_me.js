269420e6bbb4d3577a5d2eb1bf9ce9d5d0938eb2
hank <hank@henrygd.me>
2016-05-19 16:33:39 -0400

    updated email relay, added facebook link

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,4 @@
 $(function() {
-
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -12,52 +11,56 @@ $(function() {
             var subject = $("input#subject").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
-            var message = $("textarea#message").val().replace(/\n/g,"<br>");
+            var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                type: "POST",
-                  url: "https://mandrillapp.com/api/1.0/messages/send.json",
-                  data: {
-                    'key': '5zFFaXjElKmZ_aDTNJfvNA',
-                    'message': {
-                      'from_email': email,
-                      "from_name": name,
-                      'to': [
-                          {
-                            'email': 'sdollman@allseascruises.com',
-                            'type': 'to'
-                          }
-                        ],
-                      'autotext': 'true',
-                      'subject': subject,
-                      'html': '<h2>New contact form submission from <span style="color:#60A066">'+name+'</span> - '+phone+'</h2><div>'+message+'</div>',
-                    }
-                  },
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<p>Your message has been sent. Thank you, " + firstName + "!</p>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<p>Sorry " + firstName + ", it seems that our mail server is not responding. Please email us directly!</p>");
-                    $('#success > .alert-danger').append('</div>');
-                },
-            })
+            function failedResponse() {
+                $("#loadingpin, #loading").hide();
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button><p>Sorry " + firstName + 
+                        ", it seems that our mail server is not responding. Please email us directly!</p></div>");
+            }
+          window.processResponse = function(data) {
+              $("#loadingpin, #loading").hide();
+              if (data.sent) {
+                // Success message
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button><p>Your message has been sent. Thank you, " + firstName + "!</p></div>");
+                //clear all fields
+                $('#contactForm').trigger("reset");
+              }
+              else {
+                failedResponse();
+              }
+          };
+          var url = 'http://emailrelay.henrygd.me/sendmail' +
+                      // your key
+                      '?key=' + "F_6wkPBmVMsorFVhCoyXCA" +
+                      // your email
+                      '&email=' + encodeURIComponent('suzidollman@gmail.com') +
+                      // sender's email
+                      '&fromEmail=' + encodeURIComponent(email) +
+                      // sender's name
+                      '&name=' + encodeURIComponent(name) +
+                      '&phone=' + encodeURIComponent(phone) +
+                      '&subject=' + encodeURIComponent(subject) +
+                      // message body
+                      '&message=' + encodeURIComponent(message) +
+                      // callback function
+                      '&callback=processResponse';
+          var script = document.createElement('script');
+          script.onerror = function() {
+            failedResponse();
+          };
+          script.src = url;
+          $("#resultinfo").html("");
+          $("#loadingpin, #loading").show();
+          document.body.appendChild(script);
         },
         filter: function() {
             return $(this).is(":visible");
